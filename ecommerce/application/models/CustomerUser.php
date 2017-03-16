78fe65873ce9048bd2293f62286c974ae1027282
NNMMs <nahla.magdy93@gmail.com>
2017-03-16 20:24:02 +0200

    adding user and products models

---
+++ ecommerce/application/models/CustomerUser.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class CustomerUser extends Application_Model_User
+{
+    
+}
+
