3120d9d4774035c32ac572137f180e65e02f858c
hossam92 <elkommy@gmail.com>
2017-03-16 10:54:53 +0200

    adding coupon and offer models, plus the database schema needed.

---
+++ ecommerce/application/controllers/OfferController.php
@@ -0,0 +1,31 @@
+<?php
+
+class OfferController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function listAction()
+    {
+//        $offers = new Application_Model_Offer();
+//        $this->view->offers = $offers->selectAll();
+//        $offers->editOffer(1, array("discount" => 991, 'start_date' => new Zend_Db_Expr('NOW()'), 'end_date' => 'NOW()'));
+    
+//        $coupon = new Application_Model_Coupon();
+//        $coupon->addCoupon(5);
+//        $coupon->updateCoupon(1, 15);
+    }
+
+
+}
+
+
+
---
+++ ecommerce/application/models/Coupon.php
@@ -0,0 +1,36 @@
+<?php
+
+class Application_Model_Coupon extends Zend_Db_Table_Abstract
+{
+    protected $_name = 'coupon';
+    
+    public function selectAll() {
+        return $this->fetchAll()->toArray();
+    }
+    
+    public function selectOne($c_id) {
+        return $this->find($c_id)->toArray()[0];
+    }
+    
+    public function addCoupon($discount_amount) {
+        $coupon = array (
+            'c_hash' => md5(bin2hex(random_bytes(60))),
+            'discount' => (int)$discount_amount
+        );
+        $row = $this->createRow($coupon);
+        return $row->save();
+    }
+    
+    public function updateCoupon($c_id, $user_id) {
+        $coupon = array (
+            'user_id' => (int)$user_id
+        );
+        return $this->update($coupon, "id= $c_id");
+    }
+    
+    public function deleteCoupon($c_id) {
+        return $this->delete("id= $c_id");
+    }
+
+}
+
