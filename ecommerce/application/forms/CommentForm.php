a03b13d3f78be600e95939b06db652aec6b7334f
MinaZakaria <engineer_minazakaria@yahoo.com>
2017-03-24 21:01:08 +0200

    design shop & customer controller (products & details)

--- ecommerce/application/forms/CommentForm.php
+++ ecommerce/application/forms/CommentForm.php
@@ -10,7 +10,9 @@ class Application_Form_CommentForm extends Zend_Form
 
         $comment = new Zend_Form_Element_Text('comment');  // <input type="text" name="fname" class="form-control">
         $comment->setLabel('Add comment: ');
-        $comment->setAttribs(array('class'=>'form-control','ROWS'=>'4','placeholder'=>'example:this is amazing product'));   //style
+        $comment->setAttribs(array('class'=>'input-xlarge','rows'=>'4','placeholder'=>'example:this is amazing product'));   //style
+        // $comment->setAttrib("rows",4);
+
 
         $submit= new Zend_Form_Element_Submit('Submit');
         $submit->setAttribs(array('class'=>'btn btn-success '));
